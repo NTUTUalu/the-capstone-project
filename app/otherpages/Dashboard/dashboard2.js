88dd46762440a52a7753c44f038e39c5116fcba8
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2024-01-14 15:11:35 +0200

    dashboard re layering

--- app/otherpages/Dashboard/dashboard2.js
+++ app/otherpages/Dashboard/dashboard2.js
@@ -29,7 +29,7 @@ export default function Dashboard() {
     <li role="presentation" class="flex-grow text-center">
       <Link
         href="#tabs-home03"
-        class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
+        class="my-2 font-semibold tracking-wide block border-x-0 border-b-2 border-t-0 border-transparent px-2 pb-3.5 pt-4 text-xs uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-amber-500 data-[te-nav-active]:text-amber-500 dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
         data-te-toggle="pill"
         data-te-target="#tabs-home03"
         data-te-nav-active
@@ -41,7 +41,7 @@ export default function Dashboard() {
     <li role="presentation" class="flex-grow text-center">
       <Link
         href="#tabs-profile03"
-        class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
+        class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-semibold tracking-wide uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-amber-500 data-[te-nav-active]:text-amber-500 dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
         data-te-toggle="pill"
         data-te-target="#tabs-profile03"
         role="tab"
